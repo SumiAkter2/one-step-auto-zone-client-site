ae6fb8b664546f91d431b1c7f4ce3ad8e59e1647
SumiAkter2 <akteralisha36@gmail.com>
2023-02-06 21:31:12 +0600

    added service

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "./About";
 import Banner from "./Banner/Banner";
+import Service from "./Service/Service";
 
 function Home() {
   return (
     <div>
       <Banner />
       <About />
+      <Service/>
     </div>
   );
 }
---
+++ src/Components/Home/Service/Service.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import Services from "./Services";
+
+const Service = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <p className="text-sm font-bold text-secondary text-center"> Service</p>
+      <h1 className="text-3xl font-bold text-center my-4">Our Service Area</h1>
+      <p className=" text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+        corporis cum hic doloremque. Esse nisi cupiditate, blanditiis alias
+      </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-12 gap-6">
+        {services.map((service) => (
+          <Services key={service._id} service={service}></Services>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Service;
