8e0dbcb3cedddebcd5edb3089a3d546d74168583
SumiAkter2 <akteralisha36@gmail.com>
2023-02-06 13:09:16 +0600

    install tailwind, daisy Ui ,

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>One step auto Zone</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#f3f4f6",
+
+          secondary: "#701a75",
+
+          accent: "#d1d5db",
+
+          neutral: "#06b6d4",
+
+          "base-100": "#FFFFFF",
+
+          info: "#6b7280",
+
+          success: "#facc15",
+
+          warning: "#d97706",
+
+          error: "#be123c",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
