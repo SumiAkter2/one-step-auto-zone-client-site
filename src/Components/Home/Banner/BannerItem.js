cefded13b022316ee90c6b4d16c2f5807f5d93cd
SumiAkter2 <akteralisha36@gmail.com>
2023-02-06 20:41:52 +0600

    banner done

--- src/Components/Home/Banner/BannerItem.js
+++ src/Components/Home/Banner/BannerItem.js
@@ -1,36 +1,34 @@
 import React from "react";
-
+import "./Banner.css";
 const BannerItem = ({ slide }) => {
+  const { img, id, prev, next } = slide;
   return (
-      <div id={`slide${slide.id}`} className="carousel-item relative w-full mb-12" style={{
-          height:'500px'}}>
-      <img src={slide.img} alt="banner-img" className="w-full rounded-xl " />
-      <div className="absolute flex justify-end transform -translate-y-1/2 left-5  top-1/4 ">
-        <h1 className="text-6xl font-bold ">
-          Lorem ipsum dolor <br /> sit amet <br />
-          elit. Amet, !
+    <div id={`slide${id}`} className="carousel-item relative w-full h-screen mb-12">
+      <div className="carousel-img">
+        <img src={img} alt="" className="w-screen rounded-xl h-screen" />
+      </div>
+      <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+        <h1 className="text-6xl font-bold text-white">
+          Affordable <br />
+          Price for Car <br />
+          Servicing
         </h1>
       </div>
-      <div className="absolute flex justify-end transform -translate-y-1/2 left-5  top-1/2 w-1/2">
-        <p className="text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis laborum
-          quasi earum ad architecto architecto
+      <div className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24 top-1/2">
+        <p className="text-xl text-white">
+          There are many variations of passages of available, but the majority
+          have suffered alteration in some form
         </p>
       </div>
-      <div className="absolute flex justify-end transform -translate-y-1/2 left-5  bottom-1/4 mt-6">
-        <button className="btn btn-secondary">Discover More</button>{" "}
-        <button
-          className=" ml-6 btn btn-primary btn-outline
-            "
-        >
-          Latest Project
-        </button>
+      <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 top-3/4">
+        <button className="btn btn-secondary mr-5">Discover More</button>
+        <button className="btn btn-outline btn-primary">New Project</button>
       </div>
-      <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-        <a href={`#slide${slide.prev}`} className="btn btn-circle mr-5">
+      <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-24">
+        <a href={`#slide${prev}`} className="btn btn-circle mr-5">
           ❮
         </a>
-        <a href={`#slide${slide.next}`} className="btn btn-circle">
+        <a href={`#slide${next}`} className="btn btn-circle">
           ❯
         </a>
       </div>
