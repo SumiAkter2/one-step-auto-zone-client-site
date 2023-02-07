7e120c1a1a3508c7223ddc1f3e7c1df31ef30ff6
SumiAkter2 <akteralisha36@gmail.com>
2023-02-07 14:19:49 +0600

    added form

--- src/Components/CheckOut/CheckOut.js
+++ src/Components/CheckOut/CheckOut.js
@@ -1,12 +1,96 @@
-import React, { useEffect, useState } from "react";
-import { useLoaderData, useParams } from "react-router-dom";
-
+import React, { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import { coolGray } from "tailwindcss/colors";
+import checkoutImg from "../../assets/images/checkout/checkout.png";
+import { AuthContext } from "../../Context/AuthProvider";
 const CheckOut = () => {
   const service = useLoaderData();
+  const user = useContext(AuthContext);
+  const handlePlaceOrder = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = `${form.firstName.value} ${form.lastName.value}`;
+    const email = user?.email || "unregistered";
+    const phone = form.phone.value;
+    const message = form.message.value;
+    const order = {
+      serviceName: service.title,
+      servicePrice: service.price,
+      serviceId: service._id,
+      name,
+      phone,
+      email,
+      message,
+    };
+    console.log(order);
+  };
   return (
     <div>
-      <h1>checkout:{service.id}</h1>
-      <h1>{service.title}</h1>
+      <div className="carousel-item relative w-full  mb-12">
+        <div className="bg-black opacity-90">
+          <img src={checkoutImg} alt="" className="w-screen rounded-xl h-56" />
+        </div>
+        <div className="absolute right-1/2 bottom-0 ">
+          <h1 className="text-xl font-bold text-white bg-secondary text-center p-4 rounded-tl-3xl rounded-tr-3xl">
+            Home/CheckOut
+          </h1>
+        </div>
+      </div>
+      <div>
+        <form
+          onSubmit={handlePlaceOrder}
+          className="border-2 rounded-lg p-12 border-orange-200 my-6"
+        >
+          <h2 className="text-4xl text-center font-bold">
+            You are about to order: {service.title}
+          </h2>
+          <h4 className="text-3xl text-center my-4 font-bold">
+            Price: $ {service.price}
+          </h4>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <input
+              name="firstName"
+              type="text"
+              placeholder="First Name"
+              className="input input-ghost w-full  input-bordered"
+            />
+            <input
+              name="lastName"
+              type="text"
+              placeholder="Last Name"
+              className="input input-ghost w-full  input-bordered"
+            />
+            <input
+              name="phone"
+              type="text"
+              placeholder="Your Phone"
+              className="input input-ghost w-full  input-bordered"
+              required
+            />
+            <input
+              name="email"
+              type="text"
+              placeholder="Your email"
+              defaultValue={user?.email}
+              className="input input-ghost w-full  input-bordered"
+              readOnly
+            />
+          </div>
+          <br />
+          <textarea
+            name="message"
+            className="textarea textarea-bordered h-24 w-full"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <br />
+          <input
+            className="btn btn-secondary w-full "
+            type="submit"
+            value="Place Your Order"
+          />
+        </form>
+      </div>
     </div>
   );
 };
