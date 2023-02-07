4b160502efec6720142dda8af38ef7e564fa4e82
SumiAkter2 <akteralisha36@gmail.com>
2023-02-07 09:35:40 +0600

    added signup page

--- src/Components/LogInPage/Login.js
+++ src/Components/LogInPage/Login.js
@@ -4,14 +4,16 @@ const Login = () => {
   return (
     <div>
       <div className="hero min-h-screen mb-12">
-        <div className="hero-content flex-col lg:flex-row justify-between">
+        <div className="hero-content flex-col lg:flex-row grid  grid-cols-2 gap-20">
           <div className=" flex justify-center items-center p-6">
             <img src={img} alt="logIn img" />
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl  shadow-slate-400 bg-base-100 p-6">
             <div className="card-body">
-                          <form className="form-control">
-                              <h1 className="text-center text-4xl font-bold mb-6">Log In Now</h1>
+              <form className="form-control">
+                <h1 className="text-center text-4xl font-bold mb-6">
+                  Log In Now
+                </h1>
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
@@ -20,7 +22,7 @@ const Login = () => {
                   placeholder="email"
                   className="input input-bordered"
                 />
-              
+
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
@@ -31,13 +33,25 @@ const Login = () => {
                 />
                 <label className="label">
                   <a href="/" className="label-text-alt link link-hover">
-                    Forgot password?
+                    Forgot password?{" "}
+                    <a className="text-secondary" href="/">
+                      Please Reset
+                    </a>
                   </a>
                 </label>
+                <div className="form-control mt-6">
+                  <button className="btn btn-secondary ">Login</button>
+                </div>
               </form>
-              <div className="form-control mt-6">
-                <button className="btn btn-secondary">Login</button>
-              </div>
+              <div className="divider ">Or Sign In With</div>
+              <button className="btn btn-secondary btn-outline">Google</button>
+
+              <p className="text-sm">
+                New to Here?{" "}
+                <a className="text-secondary" href="/signUp">
+                  Please Sign Up
+                </a>{" "}
+              </p>
             </div>
           </div>
         </div>
--- src/Components/LogInPage/SignUp.js
+++ src/Components/LogInPage/SignUp.js
@@ -1,9 +1,60 @@
 import React from "react";
-
+import img from "../../assets/images/login/login.svg";
 const SignUp = () => {
   return (
-    <div>
-      <h1>sign</h1>
+    <div className="hero min-h-screen mb-12">
+      <div className="hero-content flex-col lg:flex-row grid  grid-cols-2 gap-20">
+        <div className=" flex justify-center items-center p-6">
+          <img src={img} alt="logIn img" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl  shadow-slate-400 bg-base-100 p-6">
+          <div className="card-body">
+            <form className="form-control">
+              <h1 className="text-center text-4xl font-bold mb-6">
+                Please Sign Up
+              </h1>
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Name"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
+
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                className="input input-bordered"
+              />
+
+              <div className="form-control mt-6">
+                <button className="btn btn-secondary ">Sign Up</button>
+              </div>
+            </form>
+            <div className="divider ">Or Sign In With</div>
+            <button className="btn btn-secondary btn-outline">Google</button>
+            <div className="divider ">Or </div>
+            <p className="">
+              Have an account?
+              <a className="text-secondary" href="/logIn">
+                Please Log In
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
