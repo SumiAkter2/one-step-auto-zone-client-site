6e96f4c40d7640e3133881f268bfd2b02e046908
SumiAkter2 <akteralisha36@gmail.com>
2023-02-11 20:06:01 +0600

    fetching products

--- src/Components/Home/PopularProduct/PopularProduct.js
+++ src/Components/Home/PopularProduct/PopularProduct.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import PopularProducts from "./PopularProducts";
 
 const PopularProduct = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("  product.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <div className="text-center my-12">
       <small className="text-secondary font-bold">Popular Products</small>
@@ -8,8 +17,15 @@ const PopularProduct = () => {
       <p className="px-6">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab eius iusto
         earum, expedita aperiam facere assumenda eum Lorem ipsum dolor sit amet.
-          </p>
-          
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-12 gap-x-4">
+        {products.map((service) => (
+          <PopularProducts
+            key={service._id}
+            service={service}
+          ></PopularProducts>
+        ))}
+      </div>
     </div>
   );
 };
