50beb9f73ef8bf5b8ee45de0f9be3ee7d60ca954
SumiAkter2 <akteralisha36@gmail.com>
2023-02-11 19:57:56 +0600

    added product.json

--- src/Components/Home/PopularProduct/PopularProduct.js
+++ src/Components/Home/PopularProduct/PopularProduct.js
@@ -2,13 +2,14 @@ import React from "react";
 
 const PopularProduct = () => {
   return (
-    <div className="text-center my-6">
+    <div className="text-center my-12">
       <small className="text-secondary font-bold">Popular Products</small>
       <h1 className="text-3xl font-bold mb-2">Browse Our Products</h1>
       <p className="px-6">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab eius iusto
         earum, expedita aperiam facere assumenda eum Lorem ipsum dolor sit amet.
-      </p>
+          </p>
+          
     </div>
   );
 };
