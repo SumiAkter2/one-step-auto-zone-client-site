d1faf9adebde1e0169942193940499a8b72018ed
SumiAkter2 <akteralisha36@gmail.com>
2023-02-19 08:37:26 +0600

    change btn on slider

--- src/Components/Home/Banner/BannerItem.js
+++ src/Components/Home/Banner/BannerItem.js
@@ -3,7 +3,10 @@ import "./Banner.css";
 const BannerItem = ({ slide }) => {
   const { img, id, prev, next } = slide;
   return (
-    <div id={`slide${id}`} className="carousel-item relative w-full h-screen mb-12">
+    <div
+      id={`slide${id}`}
+      className="carousel-item relative w-full h-screen mb-12"
+    >
       <div className="carousel-img">
         <img src={img} alt="" className="w-screen rounded-xl h-screen" />
       </div>
@@ -15,7 +18,7 @@ const BannerItem = ({ slide }) => {
         </h1>
       </div>
       <div className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24 top-1/2">
-        <p className="text-xl text-white">
+        <p className="text-xl  text-primary">
           There are many variations of passages of available, but the majority
           have suffered alteration in some form
         </p>
@@ -25,10 +28,10 @@ const BannerItem = ({ slide }) => {
         <button className="btn btn-outline btn-primary">New Project</button>
       </div>
       <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-24">
-        <a href={`#slide${prev}`} className="btn btn-circle mr-5">
+        <a href={`#slide${prev}`} className="btn btn-circle btn-primary mr-5">
           ❮
         </a>
-        <a href={`#slide${next}`} className="btn btn-circle">
+        <a href={`#slide${next}`} className="btn btn-circle btn-primary">
           ❯
         </a>
       </div>
